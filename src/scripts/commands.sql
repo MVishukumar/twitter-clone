26bb9893932ef951583b3ee9b48e55a35ffe0ae0
MVishukumar <beingvishu@gmail.com>
2020-02-02 15:10:54 +0530

    Added dockerfile.

--- src/scripts/commands.sql
+++ src/scripts/commands.sql
@@ -1,3 +1,13 @@
+--Run mysql docker
+docker run --name m-mysql -p 3306:3306 -h localhost -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
+
+--Enter into the docker mysql shell
+docker exec -it m-mysql bash
+
+--Connect to databse
+mysql -u root -p
+
+--Below commands will handle database creation, user creation and user privileges
 create database twitter_clone;
 
 use twitter_clone;
@@ -8,3 +18,5 @@ GRANT ALL PRIVILEGES ON twitter_clone.* TO 'appuser'@'%';
 
 
 FLUSH PRIVILEGES;
+
+COMMIT;
